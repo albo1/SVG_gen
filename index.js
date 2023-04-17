51f755dc58ab363a995fd3ab5d910709fc5cf790
alboy77 <al.boucetta@yahoo.com>
2023-04-17 17:46:13 -0400

    questions done

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
 const questions = [ 
     {
-        type: ''
+        type: 'maxlength-input',
+        name: 'text',
+        message: 'What letters would you like to use? (max 3 characters)',
+        maxLength: 3
+    },
+    {
+        type: 'input',
+        name: 'textcolor',
+        message: 'What text color would you like to use?',
+    },
+    {
+        type: 'list',
+        name: 'shapes',
+        choices: ['circle', 'square', 'triangle'],
+    },
+    {
+        type: 'input',
+        name: 'shape-color',
+        message: 'What color would you like your shape to be?',
     }
-]
+];
+
+// kk function to create svg?? 
