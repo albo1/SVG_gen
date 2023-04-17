c65874ea1a464ec60fa49e4fa6cfda7bd23e2110
alboy77 <al.boucetta@yahoo.com>
2023-04-17 17:39:50 -0400

    thanks to class i am adding max length promp or something

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const makeShape = require('./lib/shapes');
+const MaxLengthInputPrompt = require('inquirer-maxlengt-input-prompt');
+
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
 const questions = [ 
     {
-        
+        type: ''
     }
 ]
